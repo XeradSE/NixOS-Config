bcd7c61eddbaa73a72ce5e01cdbb4f6b834ee896
Kylian Betuel <betuelkylian17@gmail.com>
2026-08-04 14:06:45 +0200

    update

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.zig" },
 
     { import = "plugins" },
   },
