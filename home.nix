deb04b1d8c52a50a559888d36cd20f63f1cf63d1
Kylian Betuel <betuelkylian17@gmail.com>
2026-08-05 21:25:28 +0200

    update

--- home.nix
+++ home.nix
@@ -17,6 +17,7 @@
     ".config/mako".source = ./dotfiles/mako;
     ".config/wofi".source = ./dotfiles/wofi;
     ".config/quickshell".source = ./dotfiles/quickshell;
+    ".config/btop".source = ./dotfiles/btop;
     ".config/yazi".source = ./dotfiles/yazi;
     ".config/nixpkgs".source = ./dotfiles/nixpkgs;
   };
