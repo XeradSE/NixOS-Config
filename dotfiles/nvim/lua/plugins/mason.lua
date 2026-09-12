9c239772d1cfa9ae15af4318fcf2084274d5afc2
Kylian Betuel <betuelkylian17@gmail.com>
2026-09-12 17:56:12 +0200

    update

---
+++ dotfiles/nvim/lua/plugins/mason.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "mason-lspconfig.nvim",
+    opts = {
+      -- Désactive l'installation automatique pour laisser Nix s'en charger
+      automatic_installation = false,
+    },
+  },
+}
