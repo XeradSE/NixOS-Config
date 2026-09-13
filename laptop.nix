407628edcb0f0c4462ff55ced1ee2aeaa2bb56c1
Kylian Betuel <betuelkylian17@gmail.com>
2026-09-13 07:57:36 +0200

    update

--- laptop.nix
+++ laptop.nix
@@ -31,11 +31,4 @@ services.tlp = {
     moonlight-qt
   ];
 
-  services.sunshine = {
-    enable = true;
-    autoStart = false; # On laisse ton hyprland.lua s'en charger pour le moment
-    capSysAdmin = true; # C'est LA ligne magique qui autorise la capture KMS silencieuse
-    openFirewall = true; # Ouvre automatiquement les ports pour Moonlight
-  };
-
 }
