6e4b83405b45d7a854c71af1d6f45631f1207702
Kylian Betuel <betuelkylian17@gmail.com>
2026-09-27 00:24:08 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -147,7 +147,7 @@ in
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  stdenv.hostPlatform.system.stateVersion = "26.05"; # Did you read the comment?
+  system.stateVersion = "26.05"; # Did you read the comment?
 
   nixpkgs.config.allowUnfree = true;
 
