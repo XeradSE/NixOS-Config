4feaf7c76b4d71524048790681570c692f7bb91b
Kylian Betuel <betuelkylian17@gmail.com>
2026-09-26 14:03:27 +0200

    update

--- dotfiles/nvim/lua/plugins/mason.lua
+++ dotfiles/nvim/lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
   -- On désactive purement et simplement tout l'écosystème Mason
-  { "williamboman/mason.nvim", enabled = false },
+  { "mason-org/mason.nvim", enabled = false },
   { "mason-org/mason-lspconfig.nvim", enabled = false },
 
   -- Si LazyVim a ajouté d'autres ponts vers Mason, on les coupe aussi
