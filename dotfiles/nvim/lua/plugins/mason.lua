1a1afab25a021deebded9860f8f3df559cd9dc52
Kylian Betuel <betuelkylian17@gmail.com>
2026-09-26 13:37:41 +0200

    update

--- dotfiles/nvim/lua/plugins/mason.lua
+++ dotfiles/nvim/lua/plugins/mason.lua
@@ -1,18 +1,8 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = function(_, opts)
-      -- Vide la liste des outils (linters/formateurs) forcés par LazyVim
-      opts.ensure_installed = {}
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      -- Coupe l'installation automatique au chargement d'un fichier
-      opts.automatic_installation = false
-      -- Vide la liste des serveurs LSP forcés
-      opts.ensure_installed = {}
-    end,
-  },
+  -- On désactive purement et simplement tout l'écosystème Mason
+  { "williamboman/mason.nvim", enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
+
+  -- Si LazyVim a ajouté d'autres ponts vers Mason, on les coupe aussi
+  { "WhoIsSethDaniel/mason-tool-installer.nvim", enabled = false },
 }
