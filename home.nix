592485e15ef2eb80276b24395ad60620d9f269f1
Kylian Betuel <betuelkylian17@gmail.com>
2026-09-26 21:38:25 +0200

    update

--- home.nix
+++ home.nix
@@ -9,7 +9,7 @@
   home.file = {
     # Exemple pour Neovim :
     ".config/nvim".source = ./dotfiles/nvim;
-    
+
     # Exemple pour Hyprland :
     ".config/hypr".source = ./dotfiles/hypr;
 
@@ -33,21 +33,21 @@
   ];
 
   # Ne change pas cette version, elle indique avec quelle version de HM tu as commencé
-  home.stateVersion = "26.05"; 
+  home.stateVersion = "26.05";
   programs.home-manager.enable = true;
 
-# 1. Le cœur de Zsh
+  # 1. Le cœur de Zsh
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    
+
     # On active les plugins classiques d'un simple clic
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    
-  shellAliases = {
+
+    shellAliases = {
       # $(hostname) récupère automatiquement le nom de la machine en cours
-      update = "sudo nixos-rebuild boot --flake ~/nixos-config#$(hostname)";
+      update = "sudo nixos-rebuild boot --flake ~/Nixos-Config#$(hostname)";
     };
 
     # On garde juste l'auto-démarrage pour ton fixe
@@ -58,16 +58,16 @@
     '';
   };
 
-    programs.zsh.oh-my-zsh = {
-      enable = true;
-      theme = "robbyrussell";
-    };
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+    theme = "robbyrussell";
+  };
 
   # 2. Les outils qui s'intègrent tout seuls à Zsh
   # Plus besoin de rajouter des 'eval "$(zoxide init zsh)"' !
   programs.zoxide.enable = true;
   programs.fzf.enable = true;
-  
+
   # 3. Un prompt moderne et ultra rapide (au lieu de bricoler ton propre thème)
   programs.starship = {
     enable = true;
@@ -76,12 +76,12 @@
 
   gtk = {
     enable = true;
-    
+
     theme = {
       # Le nom exact tel qu'il apparaît dans le système
-      name = "Breeze"; 
+      name = "Breeze";
       # Le paquet NixOS correspondant
-      package = pkgs.kdePackages.breeze-gtk; 
+      package = pkgs.kdePackages.breeze-gtk;
     };
 
     iconTheme = {
@@ -95,10 +95,10 @@
     name = "Vimix-cursors"; # Le nom du dossier généré par le thème
     package = pkgs.vimix-cursors;
     size = 24;
-    
+
     # Home Manager va automatiquement exporter XCURSOR_THEME et XCURSOR_SIZE
     gtk.enable = true;
-    x11.enable = true; 
+    x11.enable = true;
   };
 
   programs.git = {
@@ -130,8 +130,9 @@
       ruff # Formatteur python
       vtsls # LSP Type/JavaScript
     ];
-    extraLuaPackages = luaPkgs: with luaPkgs; [
-      magick
-    ];
+    extraLuaPackages =
+      luaPkgs: with luaPkgs; [
+        magick
+      ];
   };
 }
